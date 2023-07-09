fff809878a3eeb6ebd5640449804b49dcfebb951
amal2428 <amald1011@gmail.com>
2023-07-09 08:24:03 +0530

    Bloc creation

---
+++ lib/core/config.dart
@@ -0,0 +1,7 @@
+const lon = '8.580637149478212';
+const lat = '76.86411280117431';
+
+const apiKey = '6b4419fd8309edc0c177f8f93073510c';
+
+const pathUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric";
